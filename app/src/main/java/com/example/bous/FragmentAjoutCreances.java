63c9dab1233dbfb691acd7cd786e2d0bb1cf8bd3
hallykeeg <roromulus22@gmail.com>
2020-08-21 03:08:37 -0400

    fab added to all components

--- app/src/main/java/com/example/bous/FragmentAjoutCreances.java
+++ app/src/main/java/com/example/bous/FragmentAjoutCreances.java
@@ -9,11 +9,11 @@ import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FragmentCreances#newInstance} factory method to
+ * Use the {@link FragmentAjoutCreances#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentCreances extends Fragment {
-    com.getbase.floatingactionbutton.FloatingActionButton fab;
+public class FragmentAjoutCreances extends Fragment {
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -23,7 +23,7 @@ public class FragmentCreances extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public FragmentCreances() {
+    public FragmentAjoutCreances() {
         // Required empty public constructor
     }
 
@@ -33,11 +33,11 @@ public class FragmentCreances extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentCreances.
+     * @return A new instance of fragment FragmentAjoutCreances.
      */
     // TODO: Rename and change types and number of parameters
-    public static FragmentCreances newInstance(String param1, String param2) {
-        FragmentCreances fragment = new FragmentCreances();
+    public static FragmentAjoutCreances newInstance(String param1, String param2) {
+        FragmentAjoutCreances fragment = new FragmentAjoutCreances();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -58,15 +58,6 @@ public class FragmentCreances extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_creances, container, false);
-        fab = view.findViewById(R.id.ajout_creances);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentAjoutCreances()).commit();
-            }
-        });
-
-        return view;
+        return inflater.inflate(R.layout.fragment_ajout_creances, container, false);
     }
 }
--- app/src/main/java/com/example/bous/FragmentDettes.java
+++ app/src/main/java/com/example/bous/FragmentDettes.java
@@ -1,12 +1,11 @@
 package com.example.bous;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +13,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class FragmentDettes extends Fragment {
+    com.getbase.floatingactionbutton.FloatingActionButton fab;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +59,14 @@ public class FragmentDettes extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dettes, container, false);
+        View view = inflater.inflate(R.layout.fragment_dettes, container, false);
+        fab = view.findViewById(R.id.ajout_dettes);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentAjoutDettes()).commit();
+            }
+        });
+        return view;
     }
 }
